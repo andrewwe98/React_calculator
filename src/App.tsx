1e6eadc93f00f2dfb13e0bc57777338c1b416ce7
Andrew Wells <andrewwe98@gmail.com>
2026-06-05 16:51:57 -0500

    green buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import { display } from './components/display/display'
-
-
+import { useState } from 'react'
 
 
 function App() {
         
 
 
-
+const [displaynum, setdisplaynum] = useState('0')
 
 
   return (
     <div className="min-h-screen w-screen bg-[radial-gradient(circle_at_top,_#fff,_#43f223)] flex items-center justify-center">
       <div className="w-60 border rounded-lg shadow-xl p-8 overflow-hidden bg-white">
-        {display()}
+        {display(displaynum)}
         <div className="grid grid-cols-4 gap-2">
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">7</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">8</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">9</button>
-          <button className="h-12 bg-orange-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">/</button>
+          <button className="h-12 bg-green-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">/</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">4</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">5</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">6</button>
-          <button className="h-12 bg-orange-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">*</button>
+          <button className="h-12 bg-green-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">*</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">1</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">2</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">3</button>
-          <button className="h-12 bg-orange-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">-</button>
+          <button className="h-12 bg-green-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">-</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">0</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">.</button>
           <button className="h-12 bg-gray-200 rounded-lg text-xl border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">=</button>
-          <button className="h-12 bg-orange-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">+</button>
+          <button className="h-12 bg-green-500 rounded-lg text-xl text-white border transition duration-150 hover:bg-opacity-90 hover:-translate-y-0.5">+</button>
         </div>
       </div>
     </div>
--- src/components/display/display.tsx
+++ src/components/display/display.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { useState } from 'react'
 
 
-const [displaynum, setdisplaynum] = useState('0')
 
 
 
-export const display = () => {
+export const display = (displaynum: string) => {
   return (
     <input type="text" className="w-full h-12 mb-4 text-right text-2xl border rounded-lg p-2 bg-transparent" placeholder={displaynum} />
   )
